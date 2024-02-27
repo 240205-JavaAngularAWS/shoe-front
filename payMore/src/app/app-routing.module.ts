3659a7dae57ba866e77fb0fd04b76f2773f1d2db
StephanDrawe <stephan.drawe@hotmail.com>
2024-02-27 10:48:30 -0600

    begin routing

--- payMore/src/app/app-routing.module.ts
+++ payMore/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './components/user-login/user-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'loginUser', component: UserLoginComponent},
+  // {path: 'loginSeller', component: LoginSellerComponent},
+  // {path: 'createAccount', component: CreateAccountComponent},
+  {path: '', pathMatch: 'full', redirectTo:'loginUser'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
