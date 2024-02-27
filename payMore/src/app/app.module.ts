a18fbcedf04b0edbd925d1861469c1d07966eb86
StephanDrawe <stephan.drawe@hotmail.com>
2024-02-27 12:21:56 -0600

    begin navbar

--- payMore/src/app/app.module.ts
+++ payMore/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserLoginComponent } from './components/user-login/user-login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserLoginComponent
+    UserLoginComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ payMore/src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+
+  toLogin(){
+      this.router.navigate(['loginUser'])
+  }
+
+  toCreateUser(){
+      this.router.navigate(['createAccount'])
+  }
+
+  constructor(private router: Router){
+
+  }
+
+}
