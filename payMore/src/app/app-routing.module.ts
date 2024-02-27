768a7b97b84e325096dd1101cca23bc6294c62bb
StephanDrawe <stephan.drawe@hotmail.com>
2024-02-27 10:49:23 -0600

    added more routing

--- payMore/src/app/app-routing.module.ts
+++ payMore/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserLoginComponent } from './components/user-login/user-login.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
 
 const routes: Routes = [
   {path: 'loginUser', component: UserLoginComponent},
-  // {path: 'loginSeller', component: LoginSellerComponent},
-  // {path: 'createAccount', component: CreateAccountComponent},
+  {path: 'createAccount', component: UserRegisterComponent},
   {path: '', pathMatch: 'full', redirectTo:'loginUser'}
 ];
 
