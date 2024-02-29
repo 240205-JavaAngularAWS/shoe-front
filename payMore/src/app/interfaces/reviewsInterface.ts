e015c9f71c3ca8b58a5c9c511284ec0cf0c6230c
StephanDrawe <stephan.drawe@hotmail.com>
2024-02-29 11:58:34 -0600

    fixed review interface

--- payMore/src/app/interfaces/reviewsInterface.ts
+++ payMore/src/app/interfaces/reviewsInterface.ts
@@ -2,16 +2,16 @@ export interface reviews {
     id: number;
     product: {
         id: number;
-        price: number;
-        color: any;
-        gender: any;
-        category: any;
-        quantity: number;
-        // seller: {};
-        // reviews: {};
-        // orders: {};
-        // imageUrl: string;
-        // description: string;
+        price?: number;
+        color?: any;
+        gender?: any;
+        category?: any;
+        quantity?: number;
+        seller?: {};
+        reviews?: {};
+        orders?: {};
+        imageUrl?: string;
+        description?: string;
     };
     content: string;
     rating: number;
