e5262627ca278de6007dd14e6626e1a2f0036596
StephanDrawe <stephan.drawe@hotmail.com>
2024-02-29 10:43:25 -0600

    updating write review

--- payMore/src/app/components/write-review/write-review.component.ts
+++ payMore/src/app/components/write-review/write-review.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-write-review',
+  standalone: true,
   templateUrl: './write-review.component.html',
+  imports: [NgbRatingModule],
   styleUrl: './write-review.component.css'
 })
 export class WriteReviewComponent {
-
+  rating = 5;
 }
