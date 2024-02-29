30277dd2a8512e23a0d7f23fd46bf94510390a80
StephanDrawe <stephan.drawe@hotmail.com>
2024-02-29 11:43:32 -0600

    finished review styling

--- payMore/src/app/components/write-review/write-review.component.ts
+++ payMore/src/app/components/write-review/write-review.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
+import { reviews } from '../../interfaces/reviewsInterface';
 
 @Component({
   selector: 'app-write-review',
@@ -10,4 +11,47 @@ import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class WriteReviewComponent {
   rating = 5;
+
+  reviews: reviews[] = [
+    {
+      id: 1,
+      product: {
+          id: 1,
+          price: 2.50,
+          color: "black",
+          gender: "female",
+          category: "sneaker",
+          quantity: 1
+      },
+      content: "hello world",
+      rating: 1
+    },
+    {
+      id: 2,
+      product: {
+          id: 2,
+          price: 2.50,
+          color: "black",
+          gender: "female",
+          category: "sneaker",
+          quantity: 1
+      },
+      content: "not good",
+      rating: 1
+    },
+    {
+      id: 3,
+      product: {
+          id: 3,
+          price: 2.50,
+          color: "black",
+          gender: "female",
+          category: "sneaker",
+          quantity: 1
+      },
+      content: "great product",
+      rating: 1
+    }
+  ]
+  
 }
---
+++ payMore/src/app/interfaces/reviewsInterface.ts
@@ -0,0 +1,18 @@
+export interface reviews {
+    id: number;
+    product: {
+        id: number;
+        price: number;
+        color: any;
+        gender: any;
+        category: any;
+        quantity: number;
+        // seller: {};
+        // reviews: {};
+        // orders: {};
+        // imageUrl: string;
+        // description: string;
+    };
+    content: string;
+    rating: number;
+}
