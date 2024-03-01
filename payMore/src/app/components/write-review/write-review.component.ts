f0227fa9c9cf2fc165d7cc7f28d8575ca487c7f9
StephanDrawe <stephan.drawe@hotmail.com>
2024-03-01 12:13:45 -0600

    write review functionality

--- payMore/src/app/components/write-review/write-review.component.ts
+++ payMore/src/app/components/write-review/write-review.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, input } from '@angular/core';
 import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 import { reviews } from '../../interfaces/reviewsInterface';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-write-review',
   standalone: true,
   templateUrl: './write-review.component.html',
-  imports: [NgbRatingModule],
+  imports: [NgbRatingModule, FormsModule],
   styleUrl: './write-review.component.css'
 })
 export class WriteReviewComponent {
-  rating = 5;
+  /*@input()*/ review: string = "";
+  // @Output() reviewChange = new EventEmitter<string>{};
+  rating: number = 5;
+  errorMessageHidden: boolean = true;
+
+  toReviewList(){
+
+    // this.review
+    // this.reviewChange.emit(this.review)
+
+    if(!this.review) {
+      this.errorMessageHidden = false;
+      return;
+    }
+
+    // this.authService.writeReview(this.review, this.rating);
+
+    this.router.navigate(['reviewList'])
+  }
+
+  constructor(private router: Router, private authService: AuthService){
+
+  }
 
   // reviews: reviews[] = [
   //   {
