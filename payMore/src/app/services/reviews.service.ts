5e50c8da5111281128fe16ddb06f0b132ed9c73d
StephanDrawe <stephan.drawe@hotmail.com>
2024-03-04 16:22:24 -0600

    started view all product

--- payMore/src/app/services/reviews.service.ts
+++ payMore/src/app/services/reviews.service.ts
@@ -10,34 +10,6 @@ export class ReviewsService {
 
   reviews: reviews[] = []
 
-  // this is for when user clicks on a single review to view it
-  // getSingleReview(id: number){
-  //   sessionStorage.removeItem("reviewId");
-
-  //   for (let review of this.reviews){
-  //     if(review.product.id === id){
-  //       sessionStorage.setItem("productId", id.toString());
-  //       console.log("productId =" + id);
-  //     }
-  //   }
-  // }
-
-  // this is for populating the reviews
-  // verifySingleReivew(): reviews[]{
-
-  //   let id: string | null = sessionStorage.getItem("reviewId");
-
-  //   if (id){
-  //     return this.reviews.filter((review)=>{
-  //       if (review.id == Number(id)){
-  //         return true;
-  //       }
-  //       return false;
-  //     })
-  //   }
-
-  //   return []
-  // }
 
   getAllReivew(id: number): Observable<reviews[]>{
 
