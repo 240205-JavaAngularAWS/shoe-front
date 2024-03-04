a515a7203217f2eee384d0b634faef43a15afba9
Ghost <mbflug@gmail.com>
2024-03-04 14:42:56 -0700

    removing uneccesary interface and sller product service

--- payMore/src/app/components/seller-portal/seller-portal.component.ts
+++ payMore/src/app/components/seller-portal/seller-portal.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { sellerList } from '../../interfaces/sellerList';
-import { SellerProductsService } from '../../services/seller-products.service';
+
 @Component({
   selector: 'seller-portal',
   templateUrl: './seller-portal.component.html',
   styleUrl: './seller-portal.component.css',
 })
 export class SellerPortalComponent {
-  lists: sellerList[] = [];
+ 
 
-  constructor(private sellerService: SellerProductsService) {}
 
   ngOnInit() {
     // we use the ngOnInit lifecycle method to control the initiliazation for our variables
