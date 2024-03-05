f98905de8866bf26b11862252cd45915af2c4f00
ericrey93 <ericreyes1109@gmail.com>
2024-03-05 13:00:52 -0600

    added cart functionality throug local storage

--- payMore/src/app/services/products.service.ts
+++ payMore/src/app/services/products.service.ts
@@ -82,6 +82,34 @@ getProductById(productId: number): Products{
   };
 }
 
+saveCart() {
+  localStorage.setItem('cartItems', JSON.stringify(this.products));
+}
+addToCart(addedProduct: any) {
+  this.products.push(addedProduct);
+  this.saveCart();
+}
+
+loadCart() {
+  this.products = JSON.parse(localStorage.getItem('cartItems') as any) || [];
+}
+
+productInCart(product: any) {
+  return this.products.findIndex((x: any) => x.id === product.id) > -1;
+}
+
+removeProduct(product: any) {
+  let index = this.products.findIndex((x: any) => x.id === product.id)
+  if(index > -1) {
+    this.products.splice(index, 1);
+    this.saveCart();
+  }
+}
+
+clearProducts() {
+  localStorage.clear();
+}
+
   constructor(private http:HttpClient) { }
 }
 
