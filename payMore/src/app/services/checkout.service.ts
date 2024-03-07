50665e44c03a8d7ac274e5316772577b30fd5600
StephanDrawe <stephan.drawe@hotmail.com>
2024-03-07 10:01:02 -0600

    fixed checkout servce

--- payMore/src/app/services/checkout.service.ts
+++ payMore/src/app/services/checkout.service.ts
@@ -7,15 +7,16 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CheckoutService {
+  private baseUrl = 'http://ec2-54-144-33-180.compute-1.amazonaws.com';
 
   constructor(private http: HttpClient) {}
 
   submitCheckout(checkoutData: any): Observable<any> {
     // Adjust the URL as per your backend endpoint for submitting the checkout data
-    return this.http.post<any>('http://localhost:8080/orders/submit', checkoutData);
+    return this.http.post<any>(`${this.baseUrl}/orders/submit`, checkoutData);
   }
 
   getOrderDetails(userId: number): Observable<any> {
-    return this.http.get<any>(`http://localhost:8080/orders/${userId}`);
+    return this.http.get<any>(`${this.baseUrl}/orders/${userId}`);
   }
 }
