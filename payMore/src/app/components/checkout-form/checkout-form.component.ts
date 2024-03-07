969000b0a93c2f5fcf9d17b15ac4ffab555977d4
jojobee333 <symone.j.bates@gmail.com>
2024-03-07 08:34:13 -0500

    Remade the entire checkout form.  Redid the logic.

--- payMore/src/app/components/checkout-form/checkout-form.component.ts
+++ payMore/src/app/components/checkout-form/checkout-form.component.ts
@@ -1,156 +1,147 @@
-import { ICreditCard } from './../../interfaces/ICreditCard';
-import { OrdersService } from './../../services/orders.service';
-// checkout.component.ts
 import { Component, OnInit } from '@angular/core';
-import { CheckoutService } from '../../services/checkout.service';
-import { AuthService } from '../../services/auth.service';
-import { IOrderItem } from '../../interfaces/IOrderItem';
-import { IAddress } from '../../interfaces/IAddress';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { IExtendedAddress } from '../../interfaces/IExtendedAddress';
-import { Products } from '../../interfaces/products';
-
-
+import { OrdersService } from './../../services/orders.service';
+import { ICreditCard } from './../../interfaces/ICreditCard';
+import { IExtendedAddress } from './../../interfaces/IExtendedAddress';
+import { Products } from './../../interfaces/products';
+import { IOrder } from '../../interfaces/IOrder';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout-form.component.html',
   styleUrls: ['./checkout-form.component.css']
 })
-export class CheckoutComponent implements OnInit{
-  shipToDifferentAddress: boolean = true;
-  orderDetails: any = {}; 
-  userId!: number; 
+export class CheckoutComponent implements OnInit {
+  shipToDifferentAddress: boolean = false;
+  userId = Number(sessionStorage.getItem("id"));
   SessionStorageLength: any;
-
-  creditCards: ICreditCard[] = [];
-  address: IAddress[] = [];
-  formGroupPayment: FormGroup;
-  formGroupShipping: FormGroup;
   products: Products[] = [];
+  cart: IOrder = { orderItems: [] };
   subTotal = 0;
-  groundShipping = 5;
   total = 0;
+  groundShipping = 5;
 
-  constructor(
-    private authService: AuthService, //
-    private checkoutService: CheckoutService,
-    private ordersService: OrdersService
-  ) {
-    this.formGroupPayment = new FormGroup({
-      firstName: new FormControl('',Validators.required),
-      lastName: new FormControl('',Validators.required),
-      cardNumber: new FormControl('',Validators.required),
-      expiration: new FormControl('',Validators.required),
-      securityCode: new FormControl('',Validators.required),
-      billingAddress: new FormControl('',Validators.required),
-      city: new FormControl('',Validators.required),
-      state: new FormControl('',Validators.required),
-      zipCode: new FormControl('',Validators.required)
-    });
-    this.formGroupShipping = new FormGroup({
-      addressText: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zipCode: new FormControl(''),
-      userId: new FormControl(''),
-      addressType: new FormControl(''),
-    });
-    if(this.ordersService.selectedProduct) {
-      //set the list of products in cart to this.products
-      this.products = [this.ordersService.selectedProduct];
-      this.ordersService.selectedProduct = null;
-    }
-    for(const product of this.products) {
-      this.subTotal += product.price;
-    }
-    this.total = this.subTotal + this.groundShipping;
-  }
+  // Payment and shipping information bound to the form via ngModel
+  payment: any = {};
+  shipping: any = {};
+
+  constructor(private orderService: OrdersService) {}
 
   ngOnInit(): void {
-    //throw new Error('Method not implemented.');
+    this.loadCart(this.userId);
   }
 
-  addAddresses(event: any): void
-  {
-    const address: any = this.formGroupPayment.value;
-    const userId: number = parseInt(sessionStorage.getItem("id") || '-1');
-    const addressVal: IExtendedAddress = {
-      addressText: address.billingAddress,
-      city: address.city,
-      state: address.state,
-      zipCode: address.zipCode,
-      userId:  userId,
+  loadCart(userId: number): void {
+    this.orderService.getCartByUserId(userId).subscribe({
+      next: (order) => {
+        this.cart = order[0];
+        this.subTotal = typeof order[0].priceTotal === 'number' ? order[0].priceTotal : 0;
+        this.total = this.subTotal + this.groundShipping;
+        this.loadProductInfo();
+      },
+      error: (error) => console.error('Error loading cart:', error)
+    });
+  }
+
+  loadProductInfo(): void {
+    this.cart.orderItems.forEach(item => {
+      this.orderService.getProductInfo(item.productId).subscribe({
+        next: (itemInfo) => {
+          item.productName = itemInfo.productName;
+          item.imageUrl = itemInfo.imageUrl;
+        },
+        error: (error) => console.error('Error loading product info:', error)
+      });
+    });
+  }
+
+  submitOrder(): void {
+    this.addAddresses(() => {
+      this.orderService.getCartByUserId(this.userId).subscribe({
+        next: (data) => {
+          const orderId = data[0]?.id;
+          if (typeof orderId === 'number') {
+            this.finalizeOrder(orderId);
+          }
+        },
+        error: (error) => console.error('Error submitting order:', error)
+      });
+    });
+  }
+
+  finalizeOrder(orderId: number): void {
+    this.orderService.submitOrder(orderId).subscribe({
+      next: (data) => {alert('Order submitted successfully:')},
+      error: (error) => console.error('Error finalizing order:', error)
+    });
+  }
+
+  addAddresses(callback: () => void): void {
+    const billingAddress = this.createBillingAddress();
+    const creditCard = this.createCreditCard(billingAddress);
+
+    this.orderService.addCreditCard(creditCard).subscribe({
+      next: (response) => {
+        console.log('Credit Card Info added successfully:', response);
+        if (this.shipToDifferentAddress) {
+          this.addShippingAddress(callback);
+        } else {
+          callback();
+        }
+      },
+      error: (error) => {
+        console.error('Error adding Credit Card Info:', error);
+        callback();
+      }
+    });
+  }
+
+  createBillingAddress(): IExtendedAddress {
+    return {
+      addressText: this.payment.billingAddress,
+      city: this.payment.city,
+      state: this.payment.state,
+      zipCode: this.payment.zipCode,
+      userId: this.userId,
       addressType: 'BILLING',
-    }
-    const value: ICreditCard = {
-      cardNumber: address.cardNumber,
-      securityCode: address.securityCode,
-      firstName: address.firstName,
-      lastName: address.lastName,
-      expirationDate: address.expiration,
-      address: addressVal,
+    };
+  }
+
+  createCreditCard(billingAddress: IExtendedAddress): ICreditCard {
+    return {
+      cardNumber: this.payment.cardNumber,
+      securityCode: this.payment.securityCode,
+      firstName: this.payment.firstName,
+      lastName: this.payment.lastName,
+      expirationDate: this.payment.expiration,
+      address: billingAddress,
       user: {
-        id: userId,
+        id: this.userId,
         firstName: '',
         lastName: '',
         userName: '',
         password: '',
         email: '',
       }
-    }
-    
-    this.ordersService.addCreditCard(value).subscribe(
-      (response: any) => {
-        console.log('Credit Card Info added successfully:', response);
-        if(this.shipToDifferentAddress) {
-          const val: IExtendedAddress = {...this.formGroupShipping.value,userId: userId, addressType: 'SHIPPING',sellerId: 5};
-          this.ordersService.addShippingAddress(val).subscribe(
-            (response: any) => {
-              console.log('Shipping Address Info added successfully:', response);
-            },
-            (error: any) => {
-              console.error('Error Shipping Address Info:', error);
-            });
-        }
+    };
+  }
+
+  addShippingAddress(callback: () => void): void {
+    const shippingAddress: IExtendedAddress = {
+      ...this.shipping,
+      userId: this.userId,
+      addressType: 'SHIPPING',
+    };
+
+    this.orderService.addShippingAddress(shippingAddress).subscribe({
+      next: (response) => {
+        console.log('Shipping Address Info added successfully:', response);
+        callback();
       },
-      (error: any) => {
-        console.error('Error adding Credit Card Info:', error);
-      });
-    /*this.ordersService.addShippingAddress(this.creditCards).subscribe()(
-      (response: any) => {
-        console.log('Shipping address added successfully:', response);
-        // Handle success response if required
-      },
-      (error) => {
-        console.error('Error adding shipping address:', error);
-        // Handle error response if required
+      error: (error) => {
+        console.error('Error Shipping Address Info:', error);
+        callback();
       }
-    );*/
+    });
   }
 }
-  
 
-
-
-
-
-// this.checkoutService.submitCheckout(checkoutData)
-//       .subscribe(
-//         (response) => {
-//           console.log('Order submitted successfully:', response);
-         
-//         },
-//         (error) => {
-//           console.error('Error submitting order:', error);
-          
-//         }
-//       );
-// submitCheckoutForm() {
-//   const checkoutData = {
-//     creditCardNumber: this.creditCardNumber,
-//     expirationDate: this.expirationDate,
-//     securityCode: this.securityCode,
-   
-//   };  
-// }
